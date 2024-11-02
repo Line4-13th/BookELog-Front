30c8e7c7bfef9a1636e74986b6d60080044d8698
kimyeonjinn <kkkl8187@naver.com>
2024-11-02 23:07:02 +0900

    feat: 카테고리 페이지 화면 구현 완성

--- src/pages/CategoryPage/CategoryPage.jsx
+++ src/pages/CategoryPage/CategoryPage.jsx
@@ -1,15 +1,32 @@
 import UpBar from "../../components/UpBar/UpBar";
 import Search from "../../components/Search/Search";
 import Category from "../../components/Search/Category";
+import "./CategoryPage.scss";
+import CategoryTitle from "../../components/Search/CategoryTitle";
+import NavBar from "../../components/Navbar";
 
 const CategoryPage = () => {
+  const categories = [
+    "시∙에세이",
+    "장편 소설",
+    "단편 소설",
+    "인문학",
+    "여행",
+    "외국어",
+    "ddd",
+  ];
   return (
     <div>
       <UpBar />
+      <CategoryTitle title="카테고리" />
       <div className="Category-container">
-        <Category />
-
         <Search />
+        <div className="grid-container">
+          {categories.map((category, index) => (
+            <Category key={index} title={category} className="grid-container" /> // Category 컴포넌트를 여러 개 렌더링
+          ))}
+          <NavBar />
+        </div>
       </div>
     </div>
   );
