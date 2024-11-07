92f6b7949a04a950ef1f259dc827b21711b9c3fc
kimyeonjinn <kkkl8187@naver.com>
2024-11-07 21:13:08 +0900

    feat: 추가 작업 중 pull을 위한 병합 충돌

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,10 @@ import Category from "./pages/CategoryPage/CategoryPage";
 import NavBar from "./components/Navbar";
 import CategoryDetail from "./pages/CategoryDetail/CategoryDetail";
 import BookDetail from "./pages/BookDetail/BookDetail";
-// import UpBar from "./components/UpBar/UpBar";
-// import React from 'react';
-import NavBar from "./components/Navbar";
-import Home from "./pages/Home/Home";
-import CurationItem from "./pages/Home/CurationItem";
 
 function App() {
   return (
     <Router>
-      {/* <UpBar /> */}
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,11 +17,6 @@ function App() {
       </Routes>
     </Router>
   );
-  <>
-    {/* <Home />
-      <NavBar /> */}
-    <CurationItem />
-  </>;
 }
 
 export default App;
